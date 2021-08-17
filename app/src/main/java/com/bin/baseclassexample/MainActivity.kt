88d81afdeff745a4f912c6dcb47e0c33da9d37cd
Hanbin <haanbin@naver.com>
2021-08-17 17:31:33 +0900

    [edit] databindingActivity로 연결

--- app/src/main/java/com/bin/baseclassexample/MainActivity.kt
+++ app/src/main/java/com/bin/baseclassexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bin.baseclassexample
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
+import com.bin.baseclassexample.databinding.DataBindingActivity
 import com.bin.baseclassexample.viewbinding.ViewBindingActivity
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -12,6 +13,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 
     fun onDataBindingAdapterClick(v: View) {
-        startActivity(Intent(this, ViewBindingActivity::class.java))
+        startActivity(Intent(this, DataBindingActivity::class.java))
     }
 }
--- app/src/main/java/com/bin/baseclassexample/databinding/DataBindingActivity.kt
+++ app/src/main/java/com/bin/baseclassexample/databinding/DataBindingActivity.kt
@@ -12,7 +12,5 @@ class DataBindingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_data_binding)
-
-        
     }
 }
